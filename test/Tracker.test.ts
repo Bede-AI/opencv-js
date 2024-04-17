71d0c91938c6164e0bdb26370ea0923e1c9d9168
Patrik Bede <patrik@bedeai.com>
2024-04-17 18:52:28 +0200

    feat: Use new compiled build

--- test/Tracker.test.ts
+++ test/Tracker.test.ts
@@ -2,7 +2,9 @@ import Jimp from "jimp";
 import path from "path";
 import { setupOpenCv, translateException } from "./cv";
 
-beforeAll(setupOpenCv);
+beforeAll(async () => {
+  await setupOpenCv();
+}, 10000); // 10 seconds timeout
 
 describe("Tracker", () => {
   it("shoud pass TS type validations", async () => {
